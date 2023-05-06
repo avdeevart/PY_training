382b28203cfbe2125e3d3ece683d3c8269e28947
Survivor of the SAO <120559747+avdeevart@users.noreply.github.com>
2023-05-06 15:29:31 +0500

    Ba zi found

--- Feng_Shui/bazi.py
+++ Feng_Shui/bazi.py
@@ -1,15 +1,27 @@
 import ephem
 
-
 # Установка географических координат места рождения
-lat, lon = '51.5074', '0.1278' # Лондон, Великобритания
+lat, lon = '51.5074', '0.1278'  # Лондон, Великобритания
 place = ephem.Observer()
 place.lat = lat
 place.lon = lon
 
-# Настройка параметров эфемерид
-ephem.Sun.compute(place)
+# Создание объекта ephem.Sun
+sun = ephem.Sun()
+
+# Настройка параметров эфемерид для объекта Sun
+sun.compute(place)
+
+# Создание объекта ephem.Body для расчета натальной карты
+body = ephem.FixedBody()
+body._ra = sun.ra
+body._dec = sun.dec
 
 # Расчет натальной карты
-birthdate = '1980/1/1 0:0:0' # Год, месяц, день, час, минута, секунда рождения
-chart = ephem.constellation(ephem.Date(birthdate))
+# Вызов метода compute() перед использованием body в ephem.constellation()
+body.compute(place)
+# Год, месяц, день, час, минута, секунда рождения
+birthdate = '1980/1/1 0:0:0'
+chart = ephem.constellation(body)
+
+print(chart)
